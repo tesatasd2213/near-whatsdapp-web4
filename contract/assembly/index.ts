8614b4e1327663a0d34cd6df0ec78533205fd22c
piontkovsk11andre1 <andrei@p10i.ru>
2022-07-12 07:06:08 +0000

    update ipfs url

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -28,7 +28,7 @@ function status(status: u32): Web4Response {
 export function web4_get(request: Web4Request): Web4Response {
     // Demonstrate serving content from IPFS
     if (request.path == "/") {
-        return bodyUrl("ipfs://bafybeifunxrv7a2um6ntxlyg6tuedkjr2my65gu3bu625jyol4j6obulr4/");
+        return bodyUrl("ipfs://bafybeiblfwnfkna3klact7t6mrd4wfsxznccof4j6tcnq4foovhxtdgpdq/");
     }
 
     // By default return 404 Not Found
